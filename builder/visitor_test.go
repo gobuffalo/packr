234cb2efe82f01c118bddc86bd714178b2a6e3c4
Mark Bates <mark@markbates.com>
2017-09-28 11:40:48 -0400

    fixed issue with boxes not picking up certain invokations of packr.NewBox

--- builder/visitor_test.go
+++ builder/visitor_test.go
@@ -13,6 +13,6 @@ func Test_Visitor(t *testing.T) {
 
 	r.Equal("example", v.Package)
 	r.Len(v.Errors, 0)
-	r.Len(v.Boxes, 3)
-	r.Equal([]string{"./assets", "./foo", "./templates"}, v.Boxes)
+	r.Len(v.Boxes, 4)
+	r.Equal([]string{"./assets", "./bar", "./foo", "./templates"}, v.Boxes)
 }
--- example/example.go
+++ example/example.go
@@ -1,17 +1,27 @@
 package example
 
-import "github.com/gobuffalo/packr"
+import (
+	"fmt"
+
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/packr"
+)
 
 var a = packr.NewBox("./foo")
 
 func init() {
 	// packr.NewBox("../idontexists")
 
-	b := "./bar"
+	b := "./baz"
 	packr.NewBox(b) // won't work, no variables allowed, only strings
 
 	foo("/templates", packr.NewBox("./templates"))
 	packr.NewBox("./assets")
+
+	r := render.New(render.Options{
+		TemplatesBox: packr.NewBox("./bar"),
+	})
+	fmt.Println(r)
 }
 
 func foo(s string, box packr.Box) {}
