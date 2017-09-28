790155f8998cbad71a9f19f2509644b495a59f49
Mark Bates <mark@markbates.com>
2017-09-28 16:50:27 -0400

    changed some copy in the generated files

--- builder/tmpl.go
+++ builder/tmpl.go
@@ -4,9 +4,9 @@ var tmpl = `package {{.Name}}
 
 import "github.com/gobuffalo/packr"
 
-// !!! GENERATE FILE !!!
+// !!! GENERATED FILE !!!
 // Do NOT hand edit this file!!
-// It is recommended that you do not check into this file into SCM.
+// It is recommended that you DO NOT check into this file into SCM.
 // We STRONGLY recommend you delete this file after you have built your
 // Go binary. You can use the "packr clean" command to clean up this,
 // and any other packr generated files.
