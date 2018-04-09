e1c38da9066ce759d1f8950858b4cf627d334ec3
Mark Bates <mark@markbates.com>
2018-04-09 12:56:49 -0400

    added verbose flag

--- packr/cmd/root.go
+++ packr/cmd/root.go
@@ -11,17 +11,24 @@ import (
 
 var input string
 var compress bool
+var verbose bool
 
 var rootCmd = &cobra.Command{
 	Use:   "packr",
 	Short: "compiles static files into Go files",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		for _, a := range args {
-			if a == "-v" {
-				builder.DebugLog = func(s string, a ...interface{}) {
-					os.Stdout.WriteString(fmt.Sprintf(s, a...))
+		if !verbose {
+			for _, a := range args {
+				if a == "-v" {
+					verbose = true
+					break
 				}
-				break
+			}
+		}
+
+		if verbose {
+			builder.DebugLog = func(s string, a ...interface{}) {
+				os.Stdout.WriteString(fmt.Sprintf(s, a...))
 			}
 		}
 		return nil
@@ -37,6 +44,7 @@ func init() {
 	pwd, _ := os.Getwd()
 	rootCmd.Flags().StringVarP(&input, "input", "i", pwd, "path to scan for packr Boxes")
 	rootCmd.Flags().BoolVarP(&compress, "compress", "z", false, "compress box contents")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "print verbose logging information")
 }
 
 // Execute the commands
