32023654cc83ad03f5085f7dc456f45b8df47963
Mark Bates <mark@markbates.com>
2018-09-07 14:51:40 -0400

    added more deprecated stuff

--- deprecated.go
+++ deprecated.go
@@ -2,8 +2,12 @@ package packr
 
 import (
 	"errors"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/gobuffalo/packr/file"
+	"github.com/gobuffalo/packr/file/resolver"
 )
 
 // File has been deprecated and file.File should be used instead
@@ -18,24 +22,59 @@ var (
 // retrieve files from either disk or the embedded
 // binary.
 func NewBox(path string) Box {
-	// TODO: deprecate
-	return New(path)
-	// var cd string
-	// if !filepath.IsAbs(path) {
-	// 	_, filename, _, _ := runtime.Caller(1)
-	// 	cd = filepath.Dir(filename)
+	b := New(path)
+	var cd string
+	if !filepath.IsAbs(path) {
+		_, filename, _, _ := runtime.Caller(1)
+		cd = filepath.Dir(filename)
+	}
+
+	// this little hack courtesy of the `-cover` flag!!
+	cov := filepath.Join("_test", "_obj_test")
+	cd = strings.Replace(cd, string(filepath.Separator)+cov, "", 1)
+	if !filepath.IsAbs(cd) && cd != "" {
+		cd = filepath.Join(GoPath(), "src", cd)
+	}
+	b.callingDir = resolver.Ident(cd)
+	return b
+}
+
+// PackBytes packs bytes for a file into a box.
+func PackBytes(box string, name string, bb []byte) {
+	panic("not implemented")
+	// gil.Lock()
+	// defer gil.Unlock()
+	// if _, ok := data[box]; !ok {
+	// 	data[box] = map[string][]byte{}
 	// }
-	//
-	// // this little hack courtesy of the `-cover` flag!!
-	// cov := filepath.Join("_test", "_obj_test")
-	// cd = strings.Replace(cd, string(filepath.Separator)+cov, "", 1)
-	// if !filepath.IsAbs(cd) && cd != "" {
-	// 	cd = filepath.Join(GoPath(), "src", cd)
+	// data[box][name] = bb
+}
+
+// PackBytesGzip packets the gzipped compressed bytes into a box.
+func PackBytesGzip(box string, name string, bb []byte) error {
+	panic("not implemented")
+	// var buf bytes.Buffer
+	// w := gzip.NewWriter(&buf)
+	// _, err := w.Write(bb)
+	// if err != nil {
+	// 	return err
 	// }
-	//
-	// return Box{
-	// 	Path:       path,
-	// 	callingDir: cd,
-	// 	data:       map[string][]byte{},
+	// err = w.Close()
+	// if err != nil {
+	// 	return err
 	// }
+	// PackBytes(box, name, buf.Bytes())
+	// return nil
+}
+
+// PackJSONBytes packs JSON encoded bytes for a file into a box.
+func PackJSONBytes(box string, name string, jbb string) error {
+	panic("not implemented")
+	// var bb []byte
+	// err := json.Unmarshal([]byte(jbb), &bb)
+	// if err != nil {
+	// 	return err
+	// }
+	// PackBytes(box, name, bb)
+	// return nil
 }
