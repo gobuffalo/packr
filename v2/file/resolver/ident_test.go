f0ddc12d8f39e4d111f9a569e956931b78e29746
Mark Bates <mark@markbates.com>
2018-12-12 16:54:19 -0500

    fixes issue with lowercase "walked" files (#131)

--- v2/file/resolver/ident_test.go
+++ v2/file/resolver/ident_test.go
@@ -7,20 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Ident_Key(t *testing.T) {
-	table := map[string]string{
-		"Foo/bar/baz":   "foo/bar/baz",
-		"Foo\\bar\\baz": "foo/bar/baz",
-	}
-
-	for in, out := range table {
-		t.Run(in, func(st *testing.T) {
-			r := require.New(st)
-			r.Equal(out, Key(in))
-		})
-	}
-}
-
 func Test_Ident_OsPath(t *testing.T) {
 	table := map[string]string{
 		"foo/bar/baz":   "foo/bar/baz",
