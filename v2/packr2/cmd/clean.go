9c2b99ce25790ea124099018fdeb1b279df38099
Mark Bates <mark@markbates.com>
2019-01-14 10:19:27 -0500

    expose the Pack and Clean functions in jam

--- v2/packr2/cmd/clean.go
+++ v2/packr2/cmd/clean.go
@@ -1,10 +1,7 @@
 package cmd
 
 import (
-	"os"
-
-	"github.com/gobuffalo/packr/v2/jam/store"
-	"github.com/pkg/errors"
+	"github.com/gobuffalo/packr/v2/jam"
 	"github.com/spf13/cobra"
 )
 
@@ -12,22 +9,8 @@ var cleanCmd = &cobra.Command{
 	Use:   "clean",
 	Short: "removes any *-packr.go files",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return clean(args...)
+		return jam.Clean(args...)
 	},
-}
-
-func clean(args ...string) error {
-	pwd, err := os.Getwd()
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	args = append(args, pwd)
-	for _, root := range args {
-		if err := store.Clean(root); err != nil {
-			return errors.WithStack(err)
-		}
-	}
-	return nil
 }
 
 func init() {
