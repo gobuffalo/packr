2065c36b3fa2798e616f4211093ecd2450852d8e
Artūrs Jānis Pētersons <arturs.j.petersons@testdevlab.com>
2019-05-12 20:46:06 +0100

    Search for boxes in vendor directory

--- v2/jam/parser/finder.go
+++ v2/jam/parser/finder.go
@@ -30,6 +30,9 @@ func (fd *finder) findAllGoFiles(dir string) ([]string, error) {
 		plog.Debug(fd, "findAllGoFiles", "dir", dir)
 
 		callback := func(path string, do *godirwalk.Dirent) error {
+			if filepath.Base(path) == "vendor" {
+				return filepath.SkipDir
+			}
 			ext := filepath.Ext(path)
 			if ext != ".go" {
 				return nil
@@ -57,9 +60,13 @@ func (fd *finder) findAllGoFiles(dir string) ([]string, error) {
 }
 
 func (fd *finder) findAllGoFilesImports(dir string) ([]string, error) {
+	return fd.findAllGoFilesImportsIn(".", dir)
+}
+
+func (fd *finder) findAllGoFilesImportsIn(path, dir string) ([]string, error) {
 	var err error
 	var names []string
-	oncer.Do(fd.key("findAllGoFilesImports", dir), func() {
+	oncer.Do(fd.key("findAllGoFilesImports", filepath.Join(dir, path)), func() {
 		ctx := build.Default
 
 		if len(ctx.SrcDirs()) == 0 {
@@ -67,8 +74,8 @@ func (fd *finder) findAllGoFilesImports(dir string) ([]string, error) {
 			return
 		}
 
-		pkg, err := ctx.ImportDir(dir, 0)
-		if strings.HasPrefix(pkg.ImportPath, "github.com/gobuffalo/packr") {
+		pkg, err := ctx.Import(path, dir, 0)
+		if strings.Contains(pkg.ImportPath, "github.com/gobuffalo/packr") {
 			return
 		}
 
@@ -90,17 +97,12 @@ func (fd *finder) findAllGoFilesImports(dir string) ([]string, error) {
 
 		plog.Debug(fd, "findAllGoFilesImports", "dir", dir)
 
-		names, _ = fd.findAllGoFiles(dir)
+		names, _ = fd.findAllGoFiles(pkg.Dir)
 		for _, n := range pkg.GoFiles {
 			names = append(names, filepath.Join(pkg.Dir, n))
 		}
 		for _, imp := range pkg.Imports {
-			if len(ctx.SrcDirs()) == 0 {
-				continue
-			}
-			d := ctx.SrcDirs()[len(ctx.SrcDirs())-1]
-			ip := filepath.Join(d, imp)
-			n, err := fd.findAllGoFilesImports(ip)
+			n, err := fd.findAllGoFilesImportsIn(imp, dir)
 			if err != nil && len(n) != 0 {
 				names = n
 				return
