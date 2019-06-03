3e72ce1a5ac19636df9f9d3437d4e0d72cc93b75
Mark Bates <mark@markbates.com>
2019-06-03 15:00:19 -0400

    adds RootsOptions to PackOptions

--- v2/jam/pack.go
+++ v2/jam/pack.go
@@ -19,6 +19,7 @@ type PackOptions struct {
 	Legacy        bool
 	StoreCmd      string
 	Roots         []string
+	RootsOptions  *parser.RootsOptions
 }
 
 // Pack the roots given + PWD
@@ -33,9 +34,15 @@ func Pack(opts PackOptions) error {
 		return err
 	}
 
-	p, err := parser.NewFromRoots(opts.Roots, &parser.RootsOptions{
-		IgnoreImports: opts.IgnoreImports,
-	})
+	if opts.RootsOptions == nil {
+		opts.RootsOptions = &parser.RootsOptions{}
+	}
+
+	if opts.IgnoreImports {
+		opts.RootsOptions.IgnoreImports = true
+	}
+
+	p, err := parser.NewFromRoots(opts.Roots, opts.RootsOptions)
 	if err != nil {
 		return err
 	}
